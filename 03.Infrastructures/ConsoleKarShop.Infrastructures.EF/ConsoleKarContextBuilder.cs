1c9e0197f0482c700b25d297743432a9a1a2a55b
Vahid <vahabkaveh@gmail.com>
2021-08-19 03:48:05 +0430

    add asp core project to solution

--- 03.Infrastructures/ConsoleKarShop.Infrastructures.EF/ConsoleKarContextBuilder.cs
+++ 03.Infrastructures/ConsoleKarShop.Infrastructures.EF/ConsoleKarContextBuilder.cs
@@ -8,7 +8,7 @@ namespace ConsoleKarShop.Infrastructures.EF
         public ConsoleKarStoreContext CreateDbContext(string[] args)
         {
             var builder = new DbContextOptionsBuilder();
-            builder.UseSqlServer(@"server=(localdb)\ProjectsV13;database=NikamoozShopDb;integrated security=true");
+            builder.UseSqlServer(@"server=(localdb)\MSSQLLocalDB;database=ConsoleKarShopDb;integrated security=true");
             return new ConsoleKarStoreContext(builder.Options);
             
         }
